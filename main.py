6973aabe6e7144936acef26361a3e83fa6f499fb
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2023-08-09 17:08:57 +0300

    CODED METHOD TO GET USER DATA Added get_item - general method to get an item via GET request with extra parameters Added get_items - method that calls get_item repetitively with changes to paging to collect all the data Added get_users - method to collect all of the users from dummy api These all need refactoring to raise errors rather than return irrelevant values

--- main.py
+++ main.py
@@ -24,12 +24,56 @@ def connectivity_test():
     else:
         print("Connected successfully!")
 
-def get_item(item):
+
+def get_item(item, args=None):
+    if args:
+        formatted_args = [f'{key}={str(args[key])}' for key in args]
+    else:
+        formatted_args = []
+    endpoint = base_address + base_endpoint + item + '?' + '&'.join([arg for arg in formatted_args])
+    response = requests.get(endpoint, headers={'app-id': app_id})
+    response_json = response.json()
+    if 'error' in response_json:
+        return None
+    return response_json
+
+
+def get_items(item):
+    data = []
+    current_page = 0
+    while True:
+        args = {'page' : current_page}
+        response = get_item(item, args)
+
+        # get_item failed to return value, there's an error communicating with the API
+        if not response:
+            return data
+
+        # obtained data is empty, meaning there's no more data to collect
+
+        if len(response['data']) == 0:
+            break
+
+        # append the obtained information and continue
+        data.extend(response['data'])
+        current_page += 1
+
+    # export the obtained data to a JSON file
+    file_name = item + 's'
+    data_json = json.dumps(data)
+
+    with open(file_name + ".json", "w") as file:
+        file.write(data_json)
+
+
+def get_users():
+    get_items('user')
 
 
 def main():
 
     connectivity_test()
+    get_users()
 
 
 if __name__ == '__main__':
